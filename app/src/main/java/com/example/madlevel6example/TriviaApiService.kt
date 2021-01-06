6dff34bc04f5983f65db42b41e8c83a18cc48254
Chris <chris.hu@hva.nl>
2021-01-06 18:57:04 +0100

    MadLevel6Example done

---
+++ app/src/main/java/com/example/madlevel6example/TriviaApiService.kt
@@ -0,0 +1,8 @@
+package com.example.madlevel6example
+
+import retrofit2.http.GET
+
+interface TriviaApiService {
+    @GET ("/random/trivia?json")
+    suspend fun getRandomNumberTrivia(): Trivia
+}
